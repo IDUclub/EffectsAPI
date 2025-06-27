2e29ac7f23ccf72f557477993c54a015bd499007
Leon <turkov.leon-2@yandex.ru>
2025-06-27 17:09:05 +0300

    feat(effects_service): - added effects_service methods structure

--- app/effects_api/effects_service.py
+++ app/effects_api/effects_service.py
@@ -1,4 +1,29 @@
+from .dto.development_dto import DevelopmentDTO, ContextDevelopmentDTO
+
+
 class EffectsService:
 
     def __init__(self):
+        pass
+
+    async def calc_project_development(self, params: DevelopmentDTO):
+        """
+        Function calculates development only for project with blocksnet
+        Args:
+            params (DevelopmentDTO):
+        Returns:
+            --
+        """
+
+        pass
+
+    async def calc_context_development(self, params: ContextDevelopmentDTO):
+        """
+        Function calculates development for context  with project with blocksnet
+        Args:
+            params (DevelopmentDTO):
+        Returns:
+            --
+        """
+
         pass
