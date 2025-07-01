1a11fabcfab2ae22e3a513e5aa80d196418c3f0f
Leon <turkov.leon-2@yandex.ru>
2025-07-01 10:29:49 +0300

    fix(dependencies): - logs fixed

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,22 +6,13 @@ from loguru import logger
 
 from app.gateways.urban_api_gateway import UrbanAPIGateway
 
+absolute_app_path = Path().absolute()
+config = Config()
+
 logger.remove()
 log_level = "INFO"
 log_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <b>{message}</b>"
 logger.add(sys.stderr, format=log_format, level=log_level, colorize=True)
-logger.add(
-    ".log",
-    level=log_level,
-    format=log_format,
-    colorize=False,
-    backtrace=True,
-    diagnose=True,
-)
-
-absolute_app_path = Path().absolute()
-config = Config()
-
 logger.add(
     absolute_app_path / f"{config.get('LOG_NAME')}",
     format=log_format,
