5839f6e265870aadf49c5959e166948840280b7c
voronapxl <trololo695@gmail.com>
2025-12-21 14:09:28 +0300

    feat(producer): 1. KafkaProducerClient init_loop set to False

--- app/common/producer_wrapper.py
+++ app/common/producer_wrapper.py
@@ -4,7 +4,7 @@ from otteroad import KafkaProducerSettings, KafkaProducerClient
 class ProducerWrapper:
     def __init__(self):
         self.producer_settings = KafkaProducerSettings.from_env()
-        self.producer_service = KafkaProducerClient(self.producer_settings)
+        self.producer_service = KafkaProducerClient(self.producer_settings, init_loop=False)
 
     async def start(self):
         await self.producer_service.start()
